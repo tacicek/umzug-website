1cdc0948f49bce80b73c60e49f38da0109bc484b
Tuncay Cicek <tuncaycicek@outlook.com>
2025-01-02 21:20:31 +0700

    Fix next.config file

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['kgchakhlyxheydizaeqa.supabase.co'],
+  },
+}
+
+module.exports = nextConfig 
